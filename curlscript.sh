064d6126f4604a76632ded8c78f59c1c9807eff1
jennawong16 <jennawong16@gmail.com>
2018-07-19 15:56:08 -0400

    sends out URL via email

--- curlscript.sh
+++ curlscript.sh
@@ -3,6 +3,8 @@
 
 IP=10.10.110.103
 PORT=8089
+FILE_PORT=8080
+ROOM=4556
 
 GATEWAY="$(curl --header "Content-Type: application/json" --request POST --data '{"janus" : "create", "transaction" : "123abc"}' https://$IP:$PORT/janus -k --insecure)"
 echo "${GATEWAY}"
@@ -16,12 +18,12 @@ echo "${PLUGIN}"
 PLUGIN_ID=$(echo $PLUGIN | awk '{print $11}')
 echo "Plugin Handle ID:" $PLUGIN_ID
 
-ENDPOINT="$(curl --header "Content-Type: application/json" --request POST --data '{"janus": "message", "transaction": "123abc", "body": { "room": 4545, "request": "create", "record": false, "is_private": true}}' https://$IP:$PORT/janus/$SESSION_ID/$PLUGIN_ID -k --insecure)"
+ENDPOINT="$(curl --header "Content-Type: application/json" --request POST --data '{"janus": "message", "transaction": "123abc", "body": { "room": '"$ROOM"', "request": "create", "record": false, "is_private": true}}' https://$IP:$PORT/janus/$SESSION_ID/$PLUGIN_ID -k --insecure)"
 echo "${ENDPOINT}"
 
-ROOM_ID=$(echo $ENDPOINT | awk '{print $19}')
-ROOM=${ROOM_ID::-1}
-echo "Room ID:" $ROOM
+#ROOM_ID=$(echo $ENDPOINT | awk '{print $19}')
+#ROOM=${ROOM_ID::-1}
+#echo "Room ID:" $ROOM
 
 # Create a mountpoint for the stream:
 # First attach the streaming plugin
@@ -29,8 +31,14 @@ PLUGIN="$(curl --header "Content-Type: application/json" --request POST --data '
 echo "${PLUGIN}"
 STREAM_PLUGIN_ID=$(echo $PLUGIN | awk '{print $11}')
 
+# send out email with URL
+URL="https://$IP:$FILE_PORT?r=$ROOM"
+EMAIL="tylerm15@gmail.com jennawong16@gmail.com"
+#export $URL
+echo "Save your boi's life: " $URL | mail -s "Videoroom URL" $EMAIL 
+
 # Now create the mountpoint:
-MOUNTPOINT_CREATE="$(curl --header "Content-Type: application/json" --request POST --data '{"janus": "message", "transaction": "123abc", "body": {"request": "create", "is_private": true, "id": 4545, "type": "rtp", "audio": true, "audiopt": 10, "audioport": 8005, "audiortpmap":"opus/48000/2", "video": true, "videoport": 8004, "videopt": 96, "videortpmap": "H264/90000", "videofmtp": "profile-level-id=42e028;packetization-mode=1"}}' https://$IP:$PORT/janus/$SESSION_ID/$STREAM_PLUGIN_ID -k --insecure)"
+MOUNTPOINT_CREATE="$(curl --header "Content-Type: application/json" --request POST --data '{"janus": "message", "transaction": "123abc", "body": {"request": "create", "is_private": true, "id": '"$ROOM"', "type": "rtp", "audio": true, "audiopt": 10, "audioport": 8005, "audiortpmap":"opus/48000/2", "video": true, "videoport": 8004, "videopt": 96, "videortpmap": "H264/90000", "videofmtp": "profile-level-id=42e028;packetization-mode=1"}}' https://$IP:$PORT/janus/$SESSION_ID/$STREAM_PLUGIN_ID -k --insecure)"
 echo "${MOUNTPOINT_CREATE}"
 
 # And finish off by having the pi start the stream:
